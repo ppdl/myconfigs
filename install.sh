b09f8053909bf4cff7456c259670297e2344fe16
yjcho <pdlpdl@naver.com>
2018-02-27 21:42:38 +0900

    modifed

--- install.sh
+++ install.sh
@@ -10,13 +10,16 @@ sudo apt-get install -y terminator
 sudo apt-get install -y zsh
 sudo apt-get install -y curl
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-chsh -s `which zsh`
+sudo chsh -s `which zsh`
 
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Vim & Zsh configurations
+# Vim configurations
 cp .vimrc ../.vimrc
-cp .zshrc ../.zshrc 
+
+# Terminator config file. Font, Color, Seperator size
+mkdir ~/.config/terminator
+cp terminator_config ~/.config/terminator/config
 
 # Install all vim plugins
 vim -c 'PluginInstall' -c 'qa!'
@@ -28,9 +31,8 @@ git clone https://github.com/powerline/fonts.git --depth=1
 ./fonts/install.sh
 rm -rf fonts
 
-# Terminator config file. Font, Color, Seperator size
-cp terminator_config ~/.config/terminator/config
-
+# Zsh setting
+cp .zshrc ../.zshrc 
 
 # Monitor Resolution setting
 resolution_type_dxd=`xdpyinfo | grep -oP 'dimensions:\s+\K\S+'`
@@ -41,9 +43,13 @@ modeline=`cvt $resolution_type_d_d | grep -oP 'Modeline \K.+'`
 com3=$?
 
 if [ "$com1" = "0" -a "$com2" = "0" -a "$com3" = "0" ]; then
-	echo 'xrandr --newmode '"$modeline"'
-	xrandr --addmode Virtual1 '"$resolution_type_dxd"'
-	xrandr --output Virtual1 --mode '"$resolution_type_dxd" >> ~/.profile
+	cat ~/.profile | grep xrandr 2>&1 /dev/null
+	if [ $? -ne 0 ]
+	then
+		echo 'xrandr --newmode '"$modeline"'
+		xrandr --addmode Virtual1 '"$resolution_type_dxd"'
+		xrandr --output Virtual1 --mode '"$resolution_type_dxd" >> ~/.profile
+	fi
 fi
 
 echo '****************************************'
